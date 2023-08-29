468d1d52962144ecad284bc7c44f2fdfa04d0398
ki woong kwon <39793972+morningup@users.noreply.github.com>
2023-08-29 15:09:58 +0900

    [Silver IV] Title: 괄호, Time: 244 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int T =in.nextInt();
+        for(int i=0; i<T; i++){
+            System.out.println(solve(in.next())); // nextLine()쓰면 안된다.
+        }
+    }
+
+    public static String solve(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            }
+            // 아래는 모두 닫는 괄호 일 경우들이다.
+            // 스택이 비어있는 경우. 즉, 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
+            else if(stack.empty()){
+                return "NO";
+            }
+            // 그 외의 경우 stack 원소를 pop 한다.
+            else{
+                stack.pop(); // )이게 나온경우
+            }
+        }
+        /*
+         * 모든 검사 마치고 스택에 잔여 요소가 있으면 여는 괄호가 많은 경우는 "NO"
+         * 스택이 비어있으면 온전한 수식이므로 "YES" 이다.
+         */
+        if(stack.empty()){
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
+    }
+
+
+}
