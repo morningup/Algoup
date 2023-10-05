e9fc924fe226eeea1727b58786b520455af15b0e
ki woong kwon <39793972+morningup@users.noreply.github.com>
2023-10-05 18:48:02 +0900

    [Bronze V] Title: A+B - 2, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Main {
+    //문제 풀이 방식: 0을 입력받으면 최근에 입력받았던 수를 지운다.
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+    }
+
+}
